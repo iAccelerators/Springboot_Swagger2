2a6f29f49d9536ee5b383aca3bb95b6a19b9abe1
iAccelerators <iaccelerators@gmail.com>
2018-07-15 18:14:34 -0500

    Initial Commit

---
+++ src/main/java/com/ramesh/springframework/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.ramesh.springframework.repositories;
+
+import com.ramesh.springframework.domain.Product;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProductRepository extends CrudRepository<Product, Integer>{
+}
---
+++ src/main/java/com/ramesh/springframework/services/ProductService.java
@@ -0,0 +1,14 @@
+package com.ramesh.springframework.services;
+
+
+import com.ramesh.springframework.domain.Product;
+
+public interface ProductService {
+    Iterable<Product> listAllProducts();
+
+    Product getProductById(Integer id);
+
+    Product saveProduct(Product product);
+
+    void deleteProduct(Integer id);
+}
